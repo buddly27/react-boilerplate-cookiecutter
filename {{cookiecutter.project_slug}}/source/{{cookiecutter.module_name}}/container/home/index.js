13c7569e93ca32017a3f561250357c3872ef4ef4
Jeremy Retailleau <jeremy.retailleau@gmail.com>
2017-07-04 10:55:13 -0700

    Add redux connector to home container

--- {{cookiecutter.project_slug}}/source/{{cookiecutter.module_name}}/container/home/index.js
+++ {{cookiecutter.project_slug}}/source/{{cookiecutter.module_name}}/container/home/index.js
@@ -4,6 +4,8 @@
  */
 
 import React from "react";
+import {connect} from "react-redux";
+import {createStructuredSelector} from "reselect";
 import {FormattedMessage} from "react-intl";
 
 import messages from "./message";
@@ -21,7 +23,7 @@ import messages from "./message";
  *
  */
 // eslint-disable-next-line react/prefer-stateless-function
-export default class Home extends React.Component {
+class Home extends React.Component {
 
     render() {
         return (
@@ -31,3 +33,20 @@ export default class Home extends React.Component {
         );
     }
 }
+
+
+const mapStateToProps = createStructuredSelector({
+});
+
+
+export function mapDispatchToProps(dispatch) {
+    return {
+        dispatch,
+    };
+}
+
+
+/**
+ * Home Container wrapped with the Redux connector.
+ */
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
